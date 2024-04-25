3c75bd34791e449d345f5d9d49d044945bb2d4c2
Ethan Woo <ethan.cpw.woo@outlook.com>
2024-04-25 16:49:43 -0400

    changed cad model for simplified dof

--- src/moving_model.py
+++ src/moving_model.py
@@ -12,21 +12,21 @@ startPos = [0,0,0]
 startOrientation = p.getQuaternionFromEuler([0,0,0])
 robot = p.loadURDF("/model/robot.urdf",startPos, startOrientation)
 
-# numJoints = p.getNumJoints(robot)
-# print(numJoints) 
-# for i in range(numJoints):
-#     print(p.getJointInfo(robot, i))
+numJoints = p.getNumJoints(robot)
+print(numJoints) 
+for i in range(numJoints):
+    print(p.getJointInfo(robot, i))
 
-mode = p.VELOCITY_CONTROL
-cam = p.getDebugVisualizerCamera()
-print(cam[8])
-print(cam[9])
-print(cam[10])
+# mode = p.VELOCITY_CONTROL
+# cam = p.getDebugVisualizerCamera()
+# print(cam[8])
+# print(cam[9])
+# print(cam[10])
 p.resetDebugVisualizerCamera(cameraDistance=0.2, cameraYaw=-60, cameraPitch=-35, cameraTargetPosition=[-0.1,0,0])
 
 #set the center of mass frame (loadURDF sets base link frame) startPos/Ornp.resetBasePositionAndOrientation(boxId, startPos, startOrientation)
 for i in range (1000):
-    p.setJointMotorControl2(robot, 1, controlMode=mode, targetVelocity=10)
+    # p.setJointMotorControl2(robot, 1, controlMode=mode, targetVelocity=10)
     p.stepSimulation()
     time.sleep(1./240.)
 
