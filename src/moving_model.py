98e79116bbec256430ceb4993a8b1d8410018560
Ethan Woo <ethan.cpw.woo@outlook.com>
2024-04-26 13:35:23 -0400

    movement figured out, ready for RL implementation

--- src/moving_model.py
+++ src/moving_model.py
@@ -17,16 +17,19 @@ print(numJoints)
 for i in range(numJoints):
     print(p.getJointInfo(robot, i))
 
-# mode = p.VELOCITY_CONTROL
+mode = p.VELOCITY_CONTROL
 # cam = p.getDebugVisualizerCamera()
 # print(cam[8])
 # print(cam[9])
 # print(cam[10])
 p.resetDebugVisualizerCamera(cameraDistance=0.2, cameraYaw=-60, cameraPitch=-35, cameraTargetPosition=[-0.1,0,0])
 
+#p.changeDynamics(robot, 8, jointLowerLimit=-60, jointUpperLimit=60)
+
 #set the center of mass frame (loadURDF sets base link frame) startPos/Ornp.resetBasePositionAndOrientation(boxId, startPos, startOrientation)
 for i in range (1000):
-    # p.setJointMotorControl2(robot, 1, controlMode=mode, targetVelocity=10)
+    p.setJointMotorControl2(robot, 9, controlMode=mode, targetVelocity=-10)
+    p.setJointMotorControl2(robot, 10, controlMode=mode, targetVelocity=-10)
     p.stepSimulation()
     time.sleep(1./240.)
 
